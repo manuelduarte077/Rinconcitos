1230012f04d95fc1d7663f1a6ae31a8ea49d3666
manuelduarte077 <carlosd99m@gmail.com>
2025-02-05 01:36:41 -0600

    Add MapView to PlaceScreen with initial location setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet } from "react-native";
+import React from "react";
+import Colors from "@/constants/Colors";
+import MapView from "react-native-maps";
 
 export default function PlaceScreen() {
   return (
-    <View>
-      <Text>PlaceScreen</Text>
-    </View>
-  )
+    <MapView
+      style={styles.container}
+      showsUserLocation={true}
+      region={{
+        latitude: 12.13282,
+        longitude: -86.2504,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+        zoom: 13,
+      }}
+    />
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.light.background,
+  },
+});
