0cf78123e67bbe68c802b990c2babe56ed86f1ba
manuelduarte077 <carlosd99m@gmail.com>
2025-02-05 01:09:40 -0600

    Migrate to Expo Router and update project structure

---
+++ constants/Colors.ts
@@ -0,0 +1,31 @@
+const primary = "#ff6b4a";
+const background = "#ffffff";
+const secondary = "#f3f3f3";
+const subText = "#d8d8d8";
+const text = "#353535";
+const tint = "#ffffff";
+const backgroundIcon = "#84d383";
+const star = "#f39a39";
+
+export default {
+  light: {
+    primary,
+    background,
+    secondary,
+    subText,
+    text,
+    tint,
+    backgroundIcon,
+    star,
+  },
+  dark: {
+    primary,
+    background,
+    secondary,
+    subText,
+    text,
+    tint,
+    backgroundIcon,
+    star,
+  },
+};
